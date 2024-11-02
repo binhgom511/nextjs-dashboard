5facf45c5c2315e112ac13e16310c7cf5be5ecdc
tb <binhgom511@gmail.com>
2024-11-02 20:54:32 +0700

    third commit

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
-    webpack: (config, options) => {
-        if (!options.dev) {
-          config.devtool = options.isServer ? false : 'your-custom-devtool'
-        }
-        return config
-      },
 }
  
 export default nextConfig
