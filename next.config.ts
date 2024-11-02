c42b336afa78f1c4295073ba0f684eefd5808b57
tb <binhgom511@gmail.com>
2024-11-02 20:52:22 +0700

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from 'next';
-
+import type { NextConfig } from 'next'
+ 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+    webpack: (config, options) => {
+        if (!options.dev) {
+          config.devtool = options.isServer ? false : 'your-custom-devtool'
+        }
+        return config
+      },
+}
+ 
+export default nextConfig
